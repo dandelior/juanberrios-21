3cc41198ee5f05ed46fbc45fe291e44e4c18923c
Juan Berrios <jberrioscifuentes@gmail.com>
2021-10-21 17:43:35 -0300

    webfont fix

--- pages/_document.js
+++ pages/_document.js
@@ -12,13 +12,16 @@ class PortfolioDocument extends Document {
           <link rel="icon" type="image/svg+xml" href="/focus.svg" />
           <link rel="alternate icon" href="/favicon.ico"></link>
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#990000"></link>
-          <link
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet"></link>
+          {/* <link
             rel="preload"
             href="/fonts/Inter.var.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
-          />
+          /> */}
         </Head>
         <body>
           <Main />
